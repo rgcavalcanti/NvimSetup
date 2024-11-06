a696658efda0c00c2d971860405db2ef55392d78
rgcavalcanti <rcavalcanti.ti@gmail.com>
2024-11-06 14:47:25 -0300

    update file structure

--- init.lua
+++ init.lua
@@ -1,12 +1,5 @@
-vim.cmd("set expandtab")
-vim.g.mapleader = " "
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -22,27 +15,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-config")
 require("lazy").setup({
   spec = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+    { import = "plugins" }
   },
   install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
-})
-
-
-vim.cmd.colorscheme "catppuccin"
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {"javascript", "html", "css", "typescript", "lua"},
-  highlight = { enable = true },
-  indent = { enable = true },
 })
---
+++ lua/vim-config.lua
@@ -0,0 +1,11 @@
+vim.cmd("set expandtab")
+vim.g.mapleader = " "
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set nu")
+vim.cmd("set relativenumber")
+vim.cmd("set scrolloff=999")
+vim.cmd("set autoindent")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
